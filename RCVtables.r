8037807a8673496ca98fe17b8dc3613dad1e8b23
laserpewpewpew <kellen-gracey@uiowa.edu>
2014-03-03 12:59:35 -0600

    3/3/2014 KJG
    Initial Commit

---
+++ RCVtables.r
@@ -0,0 +1,8 @@
+###################################
+# Kellen Gracey                   #
+# Department of Political Science #
+# University of Iowa              #
+# Ranked Choice Voting Results    #
+###################################
+
+mydata <- read.dta("Updated.dta")
