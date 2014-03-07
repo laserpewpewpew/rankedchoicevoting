76e0eae6743a6cfa1b1e49492dbf23d984a8ee00
laserpewpewpew <kellen-gracey@uiowa.edu>
2014-03-06 19:13:36 -0600

    kjg 3/6/14

--- RCVtables.r
+++ RCVtables.r
@@ -24,6 +24,11 @@ mydata$newedu[mydata$education=="post-graduate"] <- 5
 mydata$newwhite[mydata$white=="non-white"] <- 0
 mydata$newwhite[mydata$white=="white"] <- 1
 
+mydata$polint[mydata$q26=="Hardly at all?"] <- 1
+mydata$polint[mydata$q26=="Only now and then, or"] <- 2
+mydata$polint[mydata$q26=="Some of the time"] <- 3
+mydata$polint[mydata$q26=="Most of the time"] <- 4
+
 ########################
 # Positive Campaigning #
 ########################
@@ -51,15 +56,15 @@ mydata$logit_positive[mydata$positive_campaigning==5] <- 1
 
 # logit
 log.pos.base <- glm(logit_positive ~ rcv_voting + newwhite + age + newedu + employed + married + democrat + republican + electoral_winner, data=mydata, family="binomial")
-log.pos.step1 <- glm(logit_positive ~ rcv_voting + newwhite + age + newedu + employed + married + democrat + republican + electoral_winner + interesting, data=mydata, family="binomial")
-log.pos.step2 <- glm(logit_positive ~ rcv_voting + newwhite + age + newedu + employed + married + democrat + republican + electoral_winner + interesting + mobilization, data=mydata, family="binomial")
-log.pos.final <- glm(logit_positive ~ rcv_voting + newwhite + age + newedu + employed + married + democrat + republican + electoral_winner + interesting + mobilization + city_satisfaction, data=mydata, family="binomial")
+log.pos.step1 <- glm(logit_positive ~ rcv_voting + newwhite + age + newedu + employed + married + democrat + republican + electoral_winner + polint, data=mydata, family="binomial")
+log.pos.step2 <- glm(logit_positive ~ rcv_voting + newwhite + age + newedu + employed + married + democrat + republican + electoral_winner + polint + mobilization, data=mydata, family="binomial")
+log.pos.final <- glm(logit_positive ~ rcv_voting + newwhite + age + newedu + employed + married + democrat + republican + electoral_winner + polint + mobilization + city_satisfaction, data=mydata, family="binomial")
 
 # ordered logit
 olog.pos.base <- polr(positive_campaigning.f ~ rcv_voting + newwhite + age + newedu + employed + married + democrat + republican + electoral_winner, data=mydata, Hess = TRUE)
-olog.pos.step1 <- polr(positive_campaigning.f ~ rcv_voting + newwhite + age + newedu + employed + married + democrat + republican + electoral_winner + interesting, data=mydata, Hess = TRUE)
-olog.pos.step2 <- polr(positive_campaigning.f ~ rcv_voting + newwhite + age + newedu + employed + married + democrat + republican + electoral_winner + interesting + mobilization, data=mydata, Hess = TRUE)
-olog.pos.final <- polr(positive_campaigning.f ~ rcv_voting + newwhite + age + newedu + employed + married + democrat + republican + electoral_winner + interesting + mobilization + city_satisfaction, data=mydata, Hess = TRUE)
+olog.pos.step1 <- polr(positive_campaigning.f ~ rcv_voting + newwhite + age + newedu + employed + married + democrat + republican + electoral_winner + polint, data=mydata, Hess = TRUE)
+olog.pos.step2 <- polr(positive_campaigning.f ~ rcv_voting + newwhite + age + newedu + employed + married + democrat + republican + electoral_winner + polint + mobilization, data=mydata, Hess = TRUE)
+olog.pos.final <- polr(positive_campaigning.f ~ rcv_voting + newwhite + age + newedu + employed + married + democrat + republican + electoral_winner + polint + mobilization + city_satisfaction, data=mydata, Hess = TRUE)
 
 ##################
 # RCV Preference #
@@ -67,9 +72,9 @@ olog.pos.final <- polr(positive_campaigning.f ~ rcv_voting + newwhite + age + ne
 
 # logit
 log.pref.base <- glm(rcv_pref ~ rcv_voting + newwhite + age + newedu + employed + married + democrat + republican + electoral_winner, data=mydata, family="binomial")
-log.pref.step1 <- glm(rcv_pref ~ rcv_voting + newwhite + age + newedu + employed + married + democrat + republican + electoral_winner + interesting, data=mydata, family="binomial")
-log.pref.step2 <- glm(rcv_pref ~ rcv_voting + newwhite + age + newedu + employed + married + democrat + republican + electoral_winner + interesting + mobilization, data=mydata, family="binomial")
-log.pref.final <- glm(rcv_pref ~ rcv_voting + newwhite + age + newedu + employed + married + democrat + republican + electoral_winner + interesting + mobilization + city_satisfaction, data=mydata, family="binomial")
+log.pref.step1 <- glm(rcv_pref ~ rcv_voting + newwhite + age + newedu + employed + married + democrat + republican + electoral_winner + polint, data=mydata, family="binomial")
+log.pref.step2 <- glm(rcv_pref ~ rcv_voting + newwhite + age + newedu + employed + married + democrat + republican + electoral_winner + polint + mobilization, data=mydata, family="binomial")
+log.pref.final <- glm(rcv_pref ~ rcv_voting + newwhite + age + newedu + employed + married + democrat + republican + electoral_winner + polint + mobilization + city_satisfaction, data=mydata, family="binomial")
 
 ##################
 # Less Criticism #
@@ -78,15 +83,15 @@ mydata$ocrit.f <- factor(mydata$ocrit, labels=c("A great deal of time", "Some of
 
 # logit
 log.crit.base <- glm(no_criticize ~ rcv_voting + newwhite + age + newedu + employed + married + democrat + republican + electoral_winner, data=mydata, family="binomial")
-log.crit.step1 <- glm(no_criticize ~ rcv_voting + newwhite + age + newedu + employed + married + democrat + republican + electoral_winner + interesting, data=mydata, family="binomial")
-log.crit.step2 <- glm(no_criticize ~ rcv_voting + newwhite + age + newedu + employed + married + democrat + republican + electoral_winner + interesting + mobilization, data=mydata, family="binomial")
-log.crit.final <- glm(no_criticize ~ rcv_voting + newwhite + age + newedu + employed + married + democrat + republican + electoral_winner + interesting + mobilization + city_satisfaction, data=mydata, family="binomial")
+log.crit.step1 <- glm(no_criticize ~ rcv_voting + newwhite + age + newedu + employed + married + democrat + republican + electoral_winner + polint, data=mydata, family="binomial")
+log.crit.step2 <- glm(no_criticize ~ rcv_voting + newwhite + age + newedu + employed + married + democrat + republican + electoral_winner + polint + mobilization, data=mydata, family="binomial")
+log.crit.final <- glm(no_criticize ~ rcv_voting + newwhite + age + newedu + employed + married + democrat + republican + electoral_winner + polint + mobilization + city_satisfaction, data=mydata, family="binomial")
 
 # ordered logit
 olog.crit.base <- polr(ocrit.f ~ rcv_voting + newwhite + age + newedu + employed + married + democrat + republican + electoral_winner, data=mydata, Hess = TRUE)
-olog.crit.step1 <- polr(ocrit.f ~ rcv_voting + newwhite + age + newedu + employed + married + democrat + republican + electoral_winner + interesting, data=mydata, Hess = TRUE)
-olog.crit.step2 <- polr(ocrit.f ~ rcv_voting + newwhite + age + newedu + employed + married + democrat + republican + electoral_winner + interesting + mobilization, data=mydata, Hess = TRUE)
-olog.crit.final <- polr(ocrit.f ~ rcv_voting + newwhite + age + newedu + employed + married + democrat + republican + electoral_winner + interesting + mobilization + city_satisfaction, data=mydata, Hess = TRUE)
+olog.crit.step1 <- polr(ocrit.f ~ rcv_voting + newwhite + age + newedu + employed + married + democrat + republican + electoral_winner + polint, data=mydata, Hess = TRUE)
+olog.crit.step2 <- polr(ocrit.f ~ rcv_voting + newwhite + age + newedu + employed + married + democrat + republican + electoral_winner + polint + mobilization, data=mydata, Hess = TRUE)
+olog.crit.final <- polr(ocrit.f ~ rcv_voting + newwhite + age + newedu + employed + married + democrat + republican + electoral_winner + polint + mobilization + city_satisfaction, data=mydata, Hess = TRUE)
 
 #########################
 # Campaign Satisfaction #
@@ -101,15 +106,15 @@ mydata$logit_satisfied[mydata$campaign_satisfaction==5] <- 1
 
 # logit
 log.sat.base <- glm(logit_satisfied ~ rcv_voting + newwhite + age + newedu + employed + married + democrat + republican + electoral_winner, data=mydata, family="binomial")
-log.sat.step1 <- glm(logit_satisfied ~ rcv_voting + newwhite + age + newedu + employed + married + democrat + republican + electoral_winner + interesting, data=mydata, family="binomial")
-log.sat.step2 <- glm(logit_satisfied ~ rcv_voting + newwhite + age + newedu + employed + married + democrat + republican + electoral_winner + interesting + mobilization, data=mydata, family="binomial")
-log.sat.final <- glm(logit_satisfied ~ rcv_voting + newwhite + age + newedu + employed + married + democrat + republican + electoral_winner + interesting + mobilization + city_satisfaction, data=mydata, family="binomial")
+log.sat.step1 <- glm(logit_satisfied ~ rcv_voting + newwhite + age + newedu + employed + married + democrat + republican + electoral_winner + polint, data=mydata, family="binomial")
+log.sat.step2 <- glm(logit_satisfied ~ rcv_voting + newwhite + age + newedu + employed + married + democrat + republican + electoral_winner + polint + mobilization, data=mydata, family="binomial")
+log.sat.final <- glm(logit_satisfied ~ rcv_voting + newwhite + age + newedu + employed + married + democrat + republican + electoral_winner + polint + mobilization + city_satisfaction, data=mydata, family="binomial")
 
 # ordered logit
 olog.sat.base <- polr(campaign_satisfaction.f ~ rcv_voting + newwhite + age + newedu + employed + married + democrat + republican + electoral_winner, data=mydata, Hess = TRUE)
-olog.sat.step1 <- polr(campaign_satisfaction.f ~ rcv_voting + newwhite + age + newedu + employed + married + democrat + republican + electoral_winner + interesting, data=mydata, Hess = TRUE)
-olog.sat.step2 <- polr(campaign_satisfaction.f ~ rcv_voting + newwhite + age + newedu + employed + married + democrat + republican + electoral_winner + interesting + mobilization, data=mydata, Hess = TRUE)
-olog.sat.final <- polr(campaign_satisfaction.f ~ rcv_voting + newwhite + age + newedu + employed + married + democrat + republican + electoral_winner + interesting + mobilization + city_satisfaction, data=mydata, Hess = TRUE)
+olog.sat.step1 <- polr(campaign_satisfaction.f ~ rcv_voting + newwhite + age + newedu + employed + married + democrat + republican + electoral_winner + polint, data=mydata, Hess = TRUE)
+olog.sat.step2 <- polr(campaign_satisfaction.f ~ rcv_voting + newwhite + age + newedu + employed + married + democrat + republican + electoral_winner + polint + mobilization, data=mydata, Hess = TRUE)
+olog.sat.final <- polr(campaign_satisfaction.f ~ rcv_voting + newwhite + age + newedu + employed + married + democrat + republican + electoral_winner + polint + mobilization + city_satisfaction, data=mydata, Hess = TRUE)
 
 #################
 # Null Findings #
@@ -120,11 +125,13 @@ mydata$candidate_satisfaction.f <- factor(mydata$candidate_satisfaction, labels=
 
 # logit
 log.fair <- glm(fairness ~ rcv_voting + newwhite + age + newedu + employed + married + democrat + republican + electoral_winner, data=mydata, family="binomial")
-log.praise <- glm(praise ~ rcv_voting + newwhite + age + newedu + employed + married + democrat + republican + electoral_winner + interesting, data=mydata, family="binomial")
+log.praise <- glm(praise ~ rcv_voting + newwhite + age + newedu + employed + married + democrat + republican + electoral_winner + polint, data=mydata, family="binomial")
+log.int <- glm(interesting ~ rcv_voting + newwhite + age + newedu + employed + married + democrat + republican + electoral_winner + polint, data=mydata, family="binomial")
+
 
 # ordered logit
 olog.info <- polr(useful_information.f ~ rcv_voting + newwhite + age + newedu + employed + married + democrat + republican + electoral_winner, data=mydata, Hess = TRUE)
-olog.cand <- polr(candidate_satisfaction.f ~ rcv_voting + newwhite + age + newedu + employed + married + democrat + republican + electoral_winner + interesting, data=mydata, Hess = TRUE)
+olog.cand <- polr(candidate_satisfaction.f ~ rcv_voting + newwhite + age + newedu + employed + married + democrat + republican + electoral_winner + polint + mayor_vote + council_vote, data=mydata, Hess = TRUE)
 
 #######################
 # Create LaTeX Tables #
@@ -134,39 +141,39 @@ library(stargazer)
 stargazer(olog.pos.base, olog.pos.step1, olog.pos.step2, olog.pos.final,
           title="Ordered Logit of Positive Campaigning", align=TRUE,
           dep.var.labels=c("Ordinal Less Negative Campaigning"),
-          covariate.labels=c("RCV", "White", "Age", "Education", "Employed", "Married", "Democrat", "Republican", "Electoral Winner", "Interesting", "Mobilization", "City Satisfaction"))
+          covariate.labels=c("RCV", "White", "Age", "Education", "Employed", "Married", "Democrat", "Republican", "Electoral Winner", "Political Interest", "Mobilization", "City Satisfaction"))
 
 stargazer(log.pos.base, log.pos.step1, log.pos.step2, log.pos.final,
           title="Logistic Regression of Positive Campaigning", align=TRUE,
           dep.var.labels=c("Dichotomous Less Negative Campaigning"),
-          covariate.labels=c("RCV", "White", "Age", "Education", "Employed", "Married", "Democrat", "Republican", "Electoral Winner", "Interesting", "Mobilization", "City Satisfaction"))
+          covariate.labels=c("RCV", "White", "Age", "Education", "Employed", "Married", "Democrat", "Republican", "Electoral Winner", "Political Interest", "Mobilization", "City Satisfaction"))
 
-stargazer(log.fair, log.praise, olog.info, olog.cand,
+stargazer(log.fair, log.praise, log.int, olog.info, olog.cand,
           title="Null and Negative Findings", align=TRUE,
-          dep.var.labels=c("Fairness", "Candidate Praise", "Useful Info.", "Candidate Choice Satisfaction"),
-          covariate.labels=c("RCV", "White", "Age", "Education", "Employed", "Married", "Democrat", "Republican", "Electoral Winner", "Interesting", "Mobilization", "City Satisfaction"))
+          dep.var.labels=c("Fairness", "Candidate Praise", "Interesting Election", "Useful Info.", "Candidate Choice Satisfaction"),
+          covariate.labels=c("RCV", "White", "Age", "Education", "Employed", "Married", "Democrat", "Republican", "Electoral Winner", "Political Interest", "Mobilization", "Mayoral Election", "Council Election"))
 
 stargazer(log.crit.base, log.crit.step1, log.crit.step2, log.crit.final,
           title="Logistic Regression of Less Criticism Between Candidates", align=TRUE,
           dep.var.labels=c("Dichotomous Less Criticism"),
-          covariate.labels=c("RCV", "White", "Age", "Education", "Employed", "Married", "Democrat", "Republican", "Electoral Winner", "Interesting", "Mobilization", "City Satisfaction"))
+          covariate.labels=c("RCV", "White", "Age", "Education", "Employed", "Married", "Democrat", "Republican", "Electoral Winner", "Political Interest", "Mobilization", "City Satisfaction"))
 
 stargazer(olog.crit.base, olog.crit.step1,olog.crit.step2, olog.crit.final, 
           title="Ordered Logit of Less Criticism Between Candidates", align=TRUE,
           dep.var.labels=c("Ordinal Less Criticism"),
-          covariate.labels=c("RCV", "White", "Age", "Education", "Employed", "Married", "Democrat", "Republican", "Electoral Winner", "Interesting", "Mobilization", "City Satisfaction"))
+          covariate.labels=c("RCV", "White", "Age", "Education", "Employed", "Married", "Democrat", "Republican", "Electoral Winner", "Political Interest", "Mobilization", "City Satisfaction"))
 
 stargazer(log.sat.base, log.sat.step1, log.sat.step2, log.sat.final, 
           title="Logistic Regression of Satisfaction with Conduct of Campaigns", align=TRUE,
           dep.var.labels=c("Dichotomous Satisfaction"),
-          covariate.labels=c("RCV", "White", "Age", "Education", "Employed", "Married", "Democrat", "Republican", "Electoral Winner", "Interesting", "Mobilization", "City Satisfaction"))
+          covariate.labels=c("RCV", "White", "Age", "Education", "Employed", "Married", "Democrat", "Republican", "Electoral Winner", "Political Interest", "Mobilization", "City Satisfaction"))
 
 stargazer(olog.sat.base, olog.sat.step1, olog.sat.step2, olog.sat.final, 
           title="Ordered Logit of Satisfaction with Conduct of Campaigns", align=TRUE,
           dep.var.labels=c("Ordinal Satisfaction"),
-          covariate.labels=c("RCV", "White", "Age", "Education", "Employed", "Married", "Democrat", "Republican", "Electoral Winner", "Interesting", "Mobilization", "City Satisfaction"))
+          covariate.labels=c("RCV", "White", "Age", "Education", "Employed", "Married", "Democrat", "Republican", "Electoral Winner", "Political Interest", "Mobilization", "City Satisfaction"))
 
 stargazer(log.pref.base, log.pref.step1, log.pref.step2, log.pref.final, 
           title="Logistic Regression of Preference for RCV System", align=TRUE,
           dep.var.labels=c("Use RCV In Other Cities"),
-          covariate.labels=c("RCV", "White", "Age", "Education", "Employed", "Married", "Democrat", "Republican", "Electoral Winner", "Interesting", "Mobilization", "City Satisfaction"))
+          covariate.labels=c("RCV", "White", "Age", "Education", "Employed", "Married", "Democrat", "Republican", "Electoral Winner", "Political Interest", "Mobilization", "City Satisfaction"))
--- candidate.R
+++ candidate.R
@@ -10,16 +10,37 @@ canddata <- read.csv("candidatercv.csv")
 
 # Housekeeping recodes
 
+canddata$white[canddata$C4race==0] <- 0
 canddata$white[canddata$C4race==1] <- 1
-canddata$white[canddata$C4race>=1] <- 0
+canddata$white[canddata$C4race==2] <- 0
+canddata$white[canddata$C4race==3] <- 0
+canddata$white[canddata$C4race==4] <- 0
+canddata$white[canddata$C4race==5] <- 0
+canddata$white[canddata$C4race==6] <- 0
+canddata$white[canddata$C4race==NA] <- 0
+
 
 canddata$employed[canddata$C1emp==1] <- 1
 canddata$employed[canddata$C1emp==2] <- 1
-canddata$employed[canddata$C1emp>=3] <- 0
+canddata$employed[canddata$C1emp==3] <- 0
+canddata$employed[canddata$C1emp==4] <- 0
+canddata$employed[canddata$C1emp==5] <- 0
+canddata$employed[canddata$C1emp==6] <- 0
+canddata$employed[canddata$C1emp==7] <- 0
+canddata$employed[canddata$C1emp==NA] <- 0
 
 canddata$female[canddata$C6female==1] <- 1
 canddata$female[canddata$C6female==0] <- 0
-canddata$female[canddata$C6female==NA] <- 0
+
+canddata$newage <- as.numeric(as.character(canddata$age))
+canddata$spending <- as.numeric(as.character(canddata$A10totspend))
+
+canddata$educ[canddata$C3educ==1] <- 1
+canddata$educ[canddata$C3educ==2] <- 2
+canddata$educ[canddata$C3educ==3] <- 3
+canddata$educ[canddata$C3educ==4] <- 4
+canddata$educ[canddata$C3educ==5] <- 5
+canddata$educ[canddata$C3educ==6] <- 6
 
 canddata$lessnegative[canddata$B6negcamps==1] <- 1
 canddata$lessnegative[canddata$B6negcamps==2] <- 2
@@ -28,6 +49,23 @@ canddata$lessnegative[canddata$B6negcamps==4] <- 4
 canddata$lessnegative[canddata$B6negcamps==5] <- 5
 canddata$lessnegative[canddata$B6negcamps==NA] <- 3
 
+canddata$fairness[canddata$B10fair==1] <- 1
+canddata$fairness[canddata$B10fair==0] <- 0
+canddata$fairness[canddata$B10fair==NA] <- 0
+
 canddata$lessnegative.f <- factor(canddata$lessnegative, labels=c("A lot more negative", "A little more negative", "About the same", "A little less negative", "A lot less negative"))
 
-olog.pos <- polr(lessnegative.f ~ rcv + C6female + age + white + C3educ + employed + B10fair, data=canddata, Hess=TRUE)
+
+#################
+# Less Negative #
+#################
+
+olog.pos <- polr(lessnegative.f ~ rcv + female + newage + white + educ + employed + fairness , data=canddata)
+summary(olog.pos)
+
+############
+# Fairness #
+############
+
+log.fair <- glm(fairness ~ rcv + female + newage + white + educ + employed + spending, data=canddata, family="binomial")
+summary(log.fair)
