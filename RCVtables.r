ee84619925992dee761e7e0c59b62646afccaecb
laserpewpewpew <kellen-gracey@uiowa.edu>
2014-05-23 20:20:55 -0500

    kjg 5/23/2014
    Attempted Coarsened Exact Match

--- RCVtables.r
+++ RCVtables.r
@@ -317,136 +317,31 @@ olog.nocedarrapids <- polr(positive_campaigning.f ~ rcv_voting + newwhite + age 
                            data=data.nocedarrapids, Hess = TRUE)
 
 require(stargazer)
-stargazer(olog.nolowell, olog.noworc, olog.noboston, olog.noseattle, olog.nodesmoines, 
-          olog.notulsa, olog.nocedarrapids)
+stargazer(olog.nolowell, olog.noworc, olog.noboston, 
+          olog.noseattle, olog.nodesmoines, 
+          olog.notulsa, olog.nocedarrapids,
+          title="Voter Perceptions of Campaign Negativity: Ordered Logit Estimates",
+          align=TRUE, no.space=TRUE,
+          covariate.labels=c("RCV","White","Age","Male",
+                             "Education","Employed","Maried",
+                             "Democrat","Republican","Electoral Winner",
+                             "Political Interest","Mobilization","City Satisfaction"),
+          notes = c("Unstandardized ordered logistic regression coefficients with robust standard errors in parentheses.","Significance levels based on two-tailed tests"))
 
 
-#############################################
-# Ease of Understanding Voting Instructions #
-#############################################
-mydata$easy.miss[mydata$q4=="Very easy"] <- 4
-mydata$easy.miss[mydata$q4=="Somewhat easy"] <- 3 
-mydata$easy.miss[mydata$q4=="Somewhat difficult"] <- 2 
-mydata$easy.miss[mydata$q4=="Very difficult"] <- 1 
-mydata$easy.miss[mydata$q4=="Dont know [VOL]"] <- NA
-mydata$easy.miss[mydata$q4=="Refused [VOL]"] <- NA
+# CEM
+cem.dat <- mydata[c(87,108,130,131,99,100,101,102,97,98,119,132,109,90)]
+cem.data <- cem.dat[!is.na(cem.dat$positive_campaigning.f),]
 
-mydata$easy.nomiss[mydata$q4=="Very easy"] <- 5
-mydata$easy.nomiss[mydata$q4=="Somewhat easy"] <- 4
-mydata$easy.nomiss[mydata$q4=="Somewhat difficult"] <- 2
-mydata$easy.nomiss[mydata$q4=="Very difficult"] <- 1
-mydata$easy.nomiss[mydata$q4=="Dont know [VOL]"] <- 3
-mydata$easy.nomiss[mydata$q4=="Refused [VOL]"] <- 3
-mydata$easy.nomiss[is.na(mydata$q4)] <- 3
+require(cem)
+vars <- c("newwhite","age","male","newedu","employed",
+          "married","democrat","republican","electoral_winner",
+          "polint","mobilization","city_satisfaction")
+imbalance(group=mydata$rcv_voting, data=mydata[vars])
 
-mydata$easy.miss.f <- factor(mydata$easy.miss, labels=c("Very Difficult", "Somewhat Difficult", "Somewhat Easy", "Very Easy"))
-mydata$easy.nomiss.f <- factor(mydata$easy.nomiss, labels=c("Very Difficult", "Somewhat Difficult", "No opinion", "Somewhat Easy", "Very Easy"))
+mat <- cem(treatment = "rcv_voting", data=mydata, drop="positive_campaigning")
 
-
-mydata$easylog[mydata$easy.miss>=4] <- 1
-mydata$easylog[mydata$easy.miss<4] <- 0
-
-mydata$easylog2[mydata$easy.miss>=3] <- 1
-mydata$easylog2[mydata$easy.miss<3] <- 0
-
-mydata$RCVeaselog[mydata$q21=="Extremely well"] <- 1
-mydata$RCVeaselog[mydata$q21=="Very well"] <- 1
-mydata$RCVeaselog[mydata$q21=="Somewhat well, or"] <- 0
-mydata$RCVeaselog[mydata$q21=="Not at all well?"] <- 0
-mydata$RCVeaselog[mydata$q21=="Dont know [VOL]"] <- 0
-mydata$RCVeaselog[mydata$q21=="Refused [VOL]"] <- 0
-
-# Dummies for black, latino, asian; not just the 'white' variable
-mydata$latino[mydata$qd4=="Yes"] <- 1
-mydata$latino[mydata$qd4=="No"] <- 0
-mydata$latino[mydata$qd4=="Refused"] <- 0
-mydata$latino[mydata$qd4=="Dont know"] <- 0
-
-mydata$black[mydata$qd3==2] <- 1
-mydata$black[mydata$qd3==1] <- 0
-mydata$black[mydata$qd3==3] <- 0
-mydata$black[mydata$qd3==4] <- 0
-mydata$black[mydata$qd3==8] <- 0
-mydata$black[mydata$qd3==9] <- 0
-
-mydata$asian[mydata$qd3==2] <- 0
-mydata$asian[mydata$qd3==1] <- 0
-mydata$asian[mydata$qd3==3] <- 1
-mydata$asian[mydata$qd3==4] <- 0
-mydata$asian[mydata$qd3==8] <- 0
-mydata$asian[mydata$qd3==9] <- 0
-
-# Income 
-
-mydata$inc[mydata$qd7=="Less than $10,000"] <- 1
-mydata$inc[mydata$qd7=="10 to under $20,000"] <- 1
-mydata$inc[mydata$qd7=="20 to under $30,000"] <- 2
-mydata$inc[mydata$qd7=="30 to under $40,000"] <- 3
-mydata$inc[mydata$qd7=="40 to under $50,000"] <- 4
-mydata$inc[mydata$qd7=="50 to under $75,000"] <- 5
-mydata$inc[mydata$qd7=="75 to under $100,000"] <- 6
-mydata$inc[mydata$qd7=="100 to under $150,000"] <- 7
-mydata$inc[mydata$qd7=="$150,000 or more"] <- 8
-mydata$inc[mydata$qd7=="Dont know [VOL]"] <- 0
-mydata$inc[mydata$qd7=="Refused [VOL]"] <- 0
-
-mydata$inc[mydata$qd7a=="No"] <- 1
-mydata$inc[mydata$qd7a=="Yes"] <- 4.38
-
-mydata$inc.miss[mydata$inc==0] <-1
-mydata$inc.miss[mydata$inc!=0] <-0
-
-# ease of understanding RCV
-
-mydata$voted[mydata$yes_voted=="Yes"] <- 1
-mydata$voted[mydata$yes_voted=="No"] <- 0
-mydata$voted <- as.numeric(mydata$voted)
-
-norcv <- mydata[mydata$rcv_voting==0 ,]
-rcv <- mydata[mydata$rcv_voting==1 ,]
-voters <- mydata[mydata$voted==1 ,]
-
-norcv.v <- norcv[norcv$voted==1 ,]
-rcv.v <- rcv[rcv$voted==1 ,]
-
-mydata$easy.f <- factor(mydata$easy, labels=c("Very Difficult", "Somewhat Difficult", "Don't know", "Somewhat Easy", "Very Easy"))
-
-# include age* rcv
-olog.easy1 <- polr(easy.nomiss.f ~ rcv_voting + black + latino + asian + age + male  + newedu + employed + married + democrat + republican + electoral_winner + polint + inc + inc.miss + rcv_voting*age, data=mydata, Hess = TRUE)
-
-
-olog.easy <- polr(easy.nomiss.f ~ black + latino + asian + age + male  + newedu + employed + married + democrat + republican + electoral_winner + polint + inc + inc.miss, data=rcv.v, Hess = TRUE)
-olog.easy2 <- polr(easy.nomiss.f ~ black + latino + asian + age + male  + newedu + employed + married + democrat + republican + electoral_winner + polint + inc + inc.miss, data=norcv.v, Hess = TRUE)
-
-olog.easy.rcv <- polr(easy.nomiss.f ~ black + latino + asian + age + male  + newedu + employed + married + democrat + republican + electoral_winner + polint + inc + inc.miss, data=rcv, Hess = TRUE)
-olog.easy.norcv<- polr(easy.nomiss.f ~ black + latino + asian + age + male  + newedu + employed + married + democrat + republican + electoral_winner + polint + inc + inc.miss, data=norcv, Hess = TRUE)
-
-
-log.easy <- glm(easylog ~ newwhite + age + male  + newedu + employed + married + democrat + republican + electoral_winner + polint + voted, data=norcv, family="binomial")
-log.easy.rcv <- glm(easylog ~ rcv_voting + newwhite + age + male  + newedu + employed + married + democrat + republican + electoral_winner + polint + voted, data=rcv, family="binomial")
-
-log.easy2 <- glm(easylog2 ~ rcv_voting + newwhite + age + male  + newedu + employed + married + democrat + republican + electoral_winner + polint + voted, data=mydata, family="binomial")
-log.easy3 <- glm(RCVeaselog ~ rcv_voting + newwhite + age + male  + newedu + employed + married + democrat + republican + electoral_winner + polint + voted, data=mydata, family="binomial")
-
-
-
-require(stargazer)
-stargazer(olog.easy1)
-stargazer(olog.easy, olog.easy2)
-stargazer(olog.easy.rcv, olog.easy.norcv)
-
-
-########################
-# TABLES TABLES TABLES #
-########################
-
-# first table: full RCV model with negative coef (w/o interaction) 
-# (show with & w/o voting control)
-# then 2 subsample models, subsetted by RCV and non-RCV (show w/ and w/o voting control)
-# then 3rd table with interaction RCV*age  (with control for voting and w/o? question)
-# then 4th, question easeRCV (specfic to RCV voters) logit 
-# predicted probabilities - variables of interest = RCV and age (interaction term)
-
-
-
+# automated progressive coarsening
+tab <- relax.cem(mat, cem.data, depth=1, perc=0.3)
+est <- att(mat, positive_campaigning ~ rcv_voting, data=mydata)
 
